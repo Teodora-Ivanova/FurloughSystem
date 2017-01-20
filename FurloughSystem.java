8bbb1ed32385e9ab419b59f8bbc44d70dacb9f96
Teodora-Ivanova <tedi_199962@abv.bg>
2017-01-20 23:01:34 +0200

    Input new methods

--- FurloughSystem.java
+++ FurloughSystem.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -10,8 +9,6 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class FurloughSystem {
-	//test commit
-	//tesiting again
 
 	public static void main(String[] args) throws IOException {
 		showTheMenu();
@@ -39,113 +36,121 @@ public class FurloughSystem {
 		}
 	}
 
-	private static void inputTheNameOfTheWantedPerson(Scanner scanner) throws IOException, FileNotFoundException {
-		System.out.println("Âúâåäåòå èìå íà ñëóæèòåë");
-		String inputtedName = scanner.nextLine();
-		int counter = countsTheLinesInTheFile();
-		// System.out.println(counter);
-		String[] reading = readArray(counter);
-		// System.out.println(Arrays.toString(reading));
-		for (int i = 0; i < counter; i += 6) {
-			if (inputtedName.equals(reading[i])) {
-				String name2 = reading[i];
-				String email2 = reading[i + 1];
-				String egn2 = reading[i + 2];
-				String initialDataOfTheFurlough2 = reading[i + 3];
-				String finalDataOfTheFurlough2 = reading[i + 4];
-				String typeOfTheFurlough2 = reading[i + 5];
-				System.out.format("%1s%35s%27s%37s%47s%43s", name2, email2, egn2, initialDataOfTheFurlough2,
-						finalDataOfTheFurlough2, typeOfTheFurlough2);
-				System.out.println();
-			}
-		}
-	}
-
 	private static void showTheMenu() {
 		System.out.println("----------------------------");
-		System.out.println("1. Çàÿâè îòïóñêà");
-		System.out.println("2. Âèæ âñè÷êè îòïóñêè");
-		System.out.println("3. Âèæ îòïóñêà íà ñëóæèòåë");
-		System.out.println("4. Ïðîìåíè ñòàòóñ íà îòïóñêà");
-		System.out.println("5. Èçõîä");
+		System.out.println("1. Заяви отпуска");
+		System.out.println("2. Виж всички отпуски");
+		System.out.println("3. Виж отпуска на служител");
+		System.out.println("4. Промени статус на отпуска");
+		System.out.println("5. Изход");
 		System.out.println("----------------------------");
 	}
 
 	private static String chooseAnOptionFromTheMenu(Scanner scanner) {
-		System.out.println("Âúâåäè èçáîð: ");
+		System.out.println("Въведи избор: ");
 		String choice = scanner.nextLine();
 		while ((!Pattern.matches("1", choice)) && (!Pattern.matches("2", choice)) && (!Pattern.matches("3", choice))
 				&& (!Pattern.matches("4", choice)) && (!Pattern.matches("5", choice))) {
-			System.out.println("Âúâåäåíèÿò îò Âàñ íîìåð íà îïöèÿ å íåêîðåêòåí! Ìîëÿ, âúâåäåòå ãî îòíîâî.");
+			System.out.println("Въведеният от Вас номер на опция е некоректен! Моля, въведете го отново.");
 			choice = scanner.nextLine();
 		}
 		return choice;
 	}
 
 	private static void inputUserName(Scanner scanner) throws FileNotFoundException {
-		System.out.println("Âúâåäåòå èìåòî ñè: ");
+		System.out.println("Въведете името си: ");
 		String name = scanner.nextLine();
-		while (!Pattern.matches(".*[À-ßà-ÿ]+.*[À-ßà-ÿ]", name)) {
-			System.out.println("Âúâåäåíîòî îò Âàñ èìå å íåêîðåêòíî. Ìîëÿ, âïèøåòå íîâî èìå.");
-			name = scanner.nextLine();
-		}
+		name = checkIfTheNameIsCorrect(scanner, name);
 		writeDownTheInputtedInformationInAFile(name);
 	}
 
-	private static void inputUserEmail(Scanner scanner) throws FileNotFoundException {
-		System.out.println("Âúâåäåòå èìåéëà ñè: ");
-		String email = scanner.nextLine();
-		while ((email.indexOf("@gmail.com") == -1) && (email.indexOf("@abv.bg") == -1)
-				&& (email.indexOf("@yahoo.com") == -1) && (email.indexOf("@mail.bg") == -1)) {
-			System.out.println("Âúâåäåíèÿò îò âàñ èìåéë å íåêîðåêòåí. Ìîëÿ, âúâåäåòå ãî îòíîâî!");
-			email = scanner.nextLine();
+	private static String checkIfTheNameIsCorrect(Scanner scanner, String name) {
+		while (!Pattern.matches(".*[А-Яа-я]+.*[А-Яа-я]", name)) {
+			System.out.println("Въведеното от Вас име е некоректно. Моля, впишете ново име.");
+			name = scanner.nextLine();
 		}
+		return name;
+	}
+
+	private static void inputUserEmail(Scanner scanner) throws FileNotFoundException {
+		System.out.println("Въведете имейла си: ");
+		String email = scanner.nextLine();
+		email = checkIfTheEmailIsCorrect(scanner, email);
 		writeDownTheInputtedInformationInAFile(email);
 	}
 
+	private static String checkIfTheEmailIsCorrect(Scanner scanner, String email) {
+		while ((email.indexOf("@gmail.com") == -1) && (email.indexOf("@abv.bg") == -1)
+				&& (email.indexOf("@yahoo.com") == -1) && (email.indexOf("@mail.bg") == -1)) {
+			System.out.println("Въведеният от вас имейл е некоректен. Моля, въведете го отново!");
+			email = scanner.nextLine();
+		}
+		return email;
+	}
+
 	private static void inputUserEGN(Scanner scanner) throws FileNotFoundException {
-		System.out.println("Âúâåäåòå åäèííèÿ ñè ãðàæäàíñêè íîìåð: ");
+		System.out.println("Въведете единния си граждански номер: ");
 		String egn = scanner.nextLine();
 		int length = egn.length();
-		while (!Pattern.matches(".*[0-9]+.*[0-9]", egn) && (length != 10)) {
-			System.out.println(
-					"Âúâåäåíèÿò îò Âàñ åäèíåí ãðàæäàíñêè íîìåð å íåêîðåêòåí. Ìîëÿ, âïèøåòå íîâ åäèíåí ãðàæäàíñêè íîìåð.");
-			egn = scanner.nextLine();
-		}
+		egn = checkIfTheEGNIsCorrect(scanner, egn, length);
 		writeDownTheInputtedInformationInAFile(egn);
 	}
 
-	private static void inputInitialDataOfTheFurlough(Scanner scanner) throws FileNotFoundException {
-		System.out.println("Âúâåäåòå íà÷àëíà äàòà çà Âàøàòà îòïóñêà: ");
-		String initialData = scanner.nextLine();
-		while (!Pattern.matches(".*[0-9]+.*[0-9]", initialData)) {
+	private static String checkIfTheEGNIsCorrect(Scanner scanner, String egn, int length) {
+		while (!Pattern.matches(".*[0-9]+.*[0-9]", egn) && (length != 10)) {
 			System.out.println(
-					"Âúâåäåíàòà îò Âàñ íà÷àëíà äàòà íà Âàøàòà îòïóñêà å íåêîðåêòíà. Ìîëÿ, âïèøåòå íîâà íà÷àëíà äàòà íà Âàøàòà îòïóñêà.");
-			initialData = scanner.nextLine();
+					"Въведеният от Вас единен граждански номер е некоректен. Моля, впишете нов единен граждански номер.");
+			egn = scanner.nextLine();
 		}
+		return egn;
+	}
+
+	private static void inputInitialDataOfTheFurlough(Scanner scanner) throws FileNotFoundException {
+		System.out.println("Въведете начална дата за Вашата отпуска: ");
+		String initialData = scanner.nextLine();
+		initialData = checkIfTheInitialDataIsCorrect(scanner, initialData);
 		writeDownTheInputtedInformationInAFile(initialData);
 
 	}
 
-	private static void inputFinalDataOfTheFurlough(Scanner scanner) {
-		System.out.println("Âúâåäåòå êðàéíà äàòà çà Âàøàòà îòïóñêà: ");
-		String finalData = scanner.nextLine();
-		while (!Pattern.matches(".*[0-9]+.*[0-9]", finalData)) {
+	private static String checkIfTheInitialDataIsCorrect(Scanner scanner, String initialData) {
+		while (!Pattern.matches(".*[0-9]+.*[0-9]", initialData)) {
 			System.out.println(
-					"Âúâåäåíàòà îò Âàñ êðàéíà äàòà íà Âàøàòà îòïóñêà å íåêîðåêòíà. Ìîëÿ, âïèøåòå íîâà êðàéíà äàòà íà Âàøàòà îòïóñêà.");
-			finalData = scanner.nextLine();
+					"Въведената от Вас начална дата на Вашата отпуска е некоректна. Моля, впишете нова начална дата на Вашата отпуска.");
+			initialData = scanner.nextLine();
 		}
+		return initialData;
+	}
+
+	private static void inputFinalDataOfTheFurlough(Scanner scanner) {
+		System.out.println("Въведете крайна дата за Вашата отпуска: ");
+		String finalData = scanner.nextLine();
+		finalData = checkIfTheFinalDataIsCorrect(scanner, finalData);
 		writeDownTheInputtedInformationInAFile(finalData);
 	}
 
+	private static String checkIfTheFinalDataIsCorrect(Scanner scanner, String finalData) {
+		while (!Pattern.matches(".*[0-9]+.*[0-9]", finalData)) {
+			System.out.println(
+					"Въведената от Вас крайна дата на Вашата отпуска е некоректна. Моля, впишете нова крайна дата на Вашата отпуска.");
+			finalData = scanner.nextLine();
+		}
+		return finalData;
+	}
+
 	private static void inputTypeOfTheFurlough(Scanner scanner) throws FileNotFoundException {
-		System.out.println("Çàäàéòå òèï íà èñêàíàòà îò Âàñ îòïóñêà (ïëàòåíà / íåïëàòåíà)");
+		System.out.println("Задайте тип на исканата от Вас отпуска (платена / неплатена)");
 		String typeOfTheFurlough = scanner.nextLine();
-		while ((!Pattern.matches("ïëàòåíà", typeOfTheFurlough)) && (!Pattern.matches("íåïëàòåíà", typeOfTheFurlough))) {
-			System.out.println("Âúâåäåíèÿò îò Âàñ òèï íà îòïóñêàòà å íåêîðåêòåí. Ìîëÿ, âïèøåòå íîâ òèï.");
+		typeOfTheFurlough = checkIfTheTypeOfTheFurloughIsCorrect(scanner, typeOfTheFurlough);
+		writeDownTheInputtedInformationInAFile(typeOfTheFurlough);
+	}
+
+	private static String checkIfTheTypeOfTheFurloughIsCorrect(Scanner scanner, String typeOfTheFurlough) {
+		while ((!Pattern.matches("платена", typeOfTheFurlough)) && (!Pattern.matches("неплатена", typeOfTheFurlough))) {
+			System.out.println("Въведеният от Вас тип на отпуската е некоректен. Моля, впишете нов тип.");
 			typeOfTheFurlough = scanner.nextLine();
 		}
-		writeDownTheInputtedInformationInAFile(typeOfTheFurlough);
+		return typeOfTheFurlough;
 	}
 
 	private static void writeDownTheInputtedInformationInAFile(String scanner) {
@@ -193,8 +198,8 @@ public class FurloughSystem {
 	}
 
 	private static void displayTheDataInATableFormat(int numberOfTheLinesInTheFile, String[] dataOfTheFile) {
-		System.out.format("%5s%30s%30s%50s%45s%40s", "Èìå", "Èìåéë", "ÅÃÍ", "Íà÷àëíà äàòà íà îïòóñêàòà",
-				"Êðàéíà äàòà íà îòïóñêàòà", "Òèï íà îòïóñêàòà");
+		System.out.printf("%5s%30s%30s%50s%45s%40s", "Име", "Имейл", "ЕГН", "Начална дата на оптуската",
+				"Крайна дата на отпуската", "Тип на отпуската");
 		System.out.println();
 		for (int i = 0; i < numberOfTheLinesInTheFile; i += 6) {
 			String name = dataOfTheFile[i];
@@ -206,6 +211,28 @@ public class FurloughSystem {
 			System.out.format("%1s%35s%27s%37s%47s%43s", name, email, egn, initialDataOfTheFurlough,
 					finalDataOfTheFurlough, typeOfTheFurlough);
 			System.out.println();
+		}
+	}
+
+	private static void inputTheNameOfTheWantedPerson(Scanner scanner) throws IOException, FileNotFoundException {
+		System.out.println("Въведете име на служител");
+		String inputtedName = scanner.nextLine();
+		int numberOfTheLinesInTheFile = countsTheLinesInTheFile();
+		// System.out.println(counter);
+		String[] readTheDataFromTheFile = readArray(numberOfTheLinesInTheFile);
+		// System.out.println(Arrays.toString(reading));
+		for (int i = 0; i < numberOfTheLinesInTheFile; i += 6) {
+			if (inputtedName.equals(readTheDataFromTheFile[i])) {
+				String name2 = readTheDataFromTheFile[i];
+				String email2 = readTheDataFromTheFile[i + 1];
+				String egn2 = readTheDataFromTheFile[i + 2];
+				String initialDataOfTheFurlough2 = readTheDataFromTheFile[i + 3];
+				String finalDataOfTheFurlough2 = readTheDataFromTheFile[i + 4];
+				String typeOfTheFurlough2 = readTheDataFromTheFile[i + 5];
+				System.out.format("%1s%35s%27s%37s%47s%43s", name2, email2, egn2, initialDataOfTheFurlough2,
+						finalDataOfTheFurlough2, typeOfTheFurlough2);
+				System.out.println();
+			}
 		}
 	}
 }
